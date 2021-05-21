b6b240dc1355dd26481159b500b1a58825c223e3
Prabha Pothen <prabha.pothen@gmail.com>
2021-05-21 12:56:26 +0200

    saving progress this far

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,30 +1,40 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
-    # @bookmark = Boookmark.new
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
   end
 
   def new
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
 
-  # def create
-  #     @list = List.new(listt_params)
-  #     if @list.save
-  #       redirect_to list(@list)
-  #     else
-  #       render "new"
-  #     end
-  # end
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
 
-  # private
+  private
 
-  # def list_params
-  #   params.require(:list).permit(:name, :bookmark, :movie)
-  # end
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
